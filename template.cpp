2bdea545351ca8decb87bfdde99f3cae3145db93
SajalJ23 <111180197+SajalJ23@users.noreply.github.com>
2022-12-30 11:10:41 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    // your code goes here
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    // fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int t;
+    cin>>t;
+    // t=1;
+    while (t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
